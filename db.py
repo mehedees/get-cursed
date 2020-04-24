492afa26578d63f9071f7760e63c5800ecf27663
Mehedee Ahmed Siddique <mehedees@live.com>
2020-04-25 00:48:24 +0600

    used specified db path in db connection.

--- db.py
+++ db.py
@@ -7,5 +7,5 @@ db_type = 'sqlite'
 
 db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bad_lang.db')
 # mydb = MySQLdb.connect(host='localhost', user='root', passwd='12345678', database='bad_lang')
-mydb = sqlite3.connect(database='bad_lang.db')
+mydb = sqlite3.connect(database=db_path)
 c = mydb.cursor()
